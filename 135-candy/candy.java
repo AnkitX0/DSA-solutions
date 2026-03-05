1eae662159fc686a18010fd1f3af0e5444bb2870
Ankit Singh <ankitsingh10a@gmail.com>
2026-03-05 17:53:41 +0530

    Time: 18 ms (5.05%) | Memory: 47.5 MB (93.15%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] left = new int [n];
+        int[] right = new int[n];
+        int candies = 0;
+
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+        
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i-1]) left[i] = left[i-1]+1;
+        }
+        for(int i = n-2; i >= 0; i--){
+            if(ratings[i] > ratings[i+1]) right[i] = right[i+1] +1;
+        }
+
+        for(int i = 0; i < n; i++){
+            candies += Math.max(left[i], right[i]);
+        }
+
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
+        return candies;
+    }
+}
