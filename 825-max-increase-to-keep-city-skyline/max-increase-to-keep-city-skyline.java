18da19d7f5641a14328ce885c7f1a4fbd107b4cb
Ankit Singh <ankitsingh10a@gmail.com>
2026-05-18 22:07:43 +0530

    Time: 1 ms (60.69%) | Memory: 45.3 MB (78.62%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -1,8 +1,6 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
 
-    //    ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
-
         ArrayList<Integer> row = new ArrayList<>();
         ArrayList<Integer> col = new ArrayList<>();
         int sum = 0;
@@ -20,17 +18,11 @@ class Solution {
             col.add(colmax);
         }
 
-        System.out.println(row.toString());
-        System.out.println(col.toString());
         for(int i = 0; i < grid[0].length; i++){
             for(int j = 0; j < grid.length; j++){ 
-
                 sum += Math.abs(grid[i][j] - Math.min(row.get(i), col.get(j)));
-
             }
         }
-
-
         return sum;
         
     }
