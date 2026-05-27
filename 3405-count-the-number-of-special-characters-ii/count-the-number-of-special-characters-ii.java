df9f6817022bc72806b8e34a9634838c809ff80c
Ankit Singh <ankitsingh10a@gmail.com>
2026-05-27 09:48:57 +0530

    Time: 33 ms (40.50%) | Memory: 48.4 MB (22.31%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -4,21 +4,13 @@ class Solution {
         HashMap <Character, Integer> CH = new HashMap<>();
         HashMap <Character, Integer> ch = new HashMap<>();
         int counter = 0;
-        
         for(int i = 0; i < word.length(); i++){
-
             char c = word.charAt(i);
             char nc = (char)(c + 32);
-
-            // System.out.println(c + " " + nc);
-
-            if(c >= 'a' && c <= 'z') ch.put(c, i);
-
-            else if (c >= 'A' && c <= 'Z' && !CH.containsKey(nc))CH.put(nc, i);
+            if(c >= 'a') ch.put(c, i);
+            else if (c <= 'Z' && !CH.containsKey(nc))CH.put(nc, i);
 
         }
-
-
         for(Character key : CH.keySet()){
             if(CH.containsKey(key) && ch.containsKey(key) && CH.get(key) > ch.get(key)) counter++;
         }
