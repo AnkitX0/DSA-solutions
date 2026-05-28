5043de4f747bec78236ae4789074c915799b7998
Ankit Singh <ankitsingh10a@gmail.com>
2026-05-28 19:10:11 +0530

    Time: 9 ms (12.31%) | Memory: 50 MB (5.36%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -6,31 +6,36 @@ class Solution {
 
         boolean validation[][] = new boolean [row][col];
 
+        int colIndex = 1;
+
         for(int i = 0; i < row; i++){
-            for(int k = 0; k < col; k++){
-                if(matrix[i][k] == 0) validation[i][k] = true;
+
+            if(matrix[i][0] == 0) colIndex = 0;
+            
+            for(int k = 1; k < col; k++){
+                    if(matrix[i][k] == 0) {
+                        matrix[0][k] = 0;
+                        matrix[i][0] = 0;
+                    }                
             }
         }
 
-
-        for(int i = 0; i < row; i++){
-            for(int k = 0; k < col; k++){
-
-                if( validation[i][k]) {
-                    for(int j = 0; j < col; j++){
-                    matrix[i][j] = 0;
-                    }
-                    for(int j = 0; j < row; j++){
-                        matrix[j][k] = 0;
-                    }
-                }
-
-
-                
-            }
-
-
-        }
+        System.out.println(Arrays.deepToString(matrix));
         
+        for(int i = row - 1; i >= 0; i--){
+            for(int k = col - 1; k >= 1; k--){
+                if(matrix[i][0] == 0 || matrix[0][k] == 0){
+                    matrix[i][k] = 0;
+                }
+            }
+            
+            if(colIndex == 0){
+                matrix[i][0] = 0;
+            }            
+        }
+
+
+
+      
     }
 }
