2e65093b997786bc5926911e71f851649cad7fa1
Ankit Singh <ankitsingh10a@gmail.com>
2026-06-01 08:20:56 +0530

    Time: 4 ms (98.44%) | Memory: 44.1 MB (96.56%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -2,19 +2,17 @@ class Solution {
     public int minimumCost(int[] cost) {
         
         PriorityQueue<Integer> pq =  new PriorityQueue<>(Collections.reverseOrder());
-
         for(int i : cost) pq.add(i);
 
         int money = 0;
         int t = 0;
-        // System.out.println(pq);
 
         while(!pq.isEmpty()){
             t++;
             if(t % 3 == 0)pq.poll();
             else money += pq.poll();
-
         }
+        
         return money;
     }
 }
