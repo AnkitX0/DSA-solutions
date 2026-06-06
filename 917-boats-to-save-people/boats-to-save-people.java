5c301f0a2d22d3180dbcafa71e0db6246d0bde13
Ankit Singh <ankitsingh10a@gmail.com>
2026-06-06 08:08:13 +0530

    Time: 20 ms (82.57%) | Memory: 56.4 MB (74.63%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        int i=0,j=people.length-1,count=0;
+        Arrays.sort(people);
+        while(i <= j){
+            if(people[i] + people[j] > limit){
+                j--;
+                count++;
+            }
+            else{
+                i++;
+                j--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
