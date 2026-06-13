41bc1e4f0564fab5bf6792583ed09719758a9875
Ankit Singh <ankitsingh10a@gmail.com>
2026-06-13 08:10:56 +0530

    Time: 209 ms (5.58%) | Memory: 46.9 MB (10.73%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        
+        String result = "";
+        for(String str : words){
+
+            int sum = 0;
+            for(char ch : str.toCharArray()){
+                sum += weights[ch-'a'];
+                System.out.println(sum);
+            }
+            result += (char)(122 - (sum%26));
+        }
+        return result;
+    }
+}
