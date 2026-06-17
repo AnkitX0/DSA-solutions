63ae948393ee46d6563ff313bbf48e24a0a78738
Ankit Singh <ankitsingh10a@gmail.com>
2026-06-17 11:15:52 +0530

    Time: 30 ms (42.31%) | Memory: 47.8 MB (88.46%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public char processStr(String s, long k) {
+        
+        long len = 0;
+
+        for (char ch : s.toCharArray()){
+
+            if(ch == '*') {if(len > 0) len--;}
+
+            else if (ch =='#') len *= 2;
+
+            else if (ch == '%') continue;
+
+            else len ++;
+        }
+        if (k >= len) return '.';
+
+        for (int i = s.length() - 1; i >=0; i--){
+            char ch = s.charAt(i);
+            
+            if(ch == '#'){
+                len /= 2;
+                if (k >= len)k -= len;
+            }
+
+            else if (ch == '*'){
+                if (len > 0) len ++;
+            }
+
+            else if (ch == '%') k = len -1 - k;
+
+            else {
+                len --;
+                if(k == len )   return ch;
+            }
+        }
+
+        return '.';
+    }
+}
