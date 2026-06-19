e629f6a9b9193a6c9e8c8460d3cdad9047bf670e
Ankit Singh <ankitsingh10a@gmail.com>
2026-06-19 19:22:50 +0530

    Time: 4 ms (96.94%) | Memory: 100.8 MB (94.25%) - LeetSync

---
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode rever = null;
+        while(slow != null){
+            ListNode next = slow.next;
+            slow.next = rever;
+            rever = slow;
+            slow = next; 
+        }
+
+        slow = head;
+        int max = 0;
+        while (rever != null) {
+            int sum = rever.val + slow.val;
+
+            if(sum > max) max = sum;
+            rever = rever.next;
+            slow = slow.next;
+        }
+
+        return max;        
+        
+    }
+}
