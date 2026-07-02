d8296d735691bd78222893326bbbb4e41d60828e
Ankit Singh <ankitsingh10a@gmail.com>
2026-07-02 08:50:33 +0530

    Time: 43 ms (27.46%) | Memory: 47.5 MB (21.72%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,44 @@
+class Solution {
+    List<List<Integer>> grid;
+    int m, n;
+    boolean visited[][];
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+
+        this.grid = grid;
+        m = grid.size();
+        n = grid.get(0).size();
+
+        int[][] healthMap = new int [m][n];
+        for(int row[] : healthMap)Arrays.fill(row, -1);
+        
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[] {0, 0, health});
+  
+        while(!que.isEmpty()){
+        
+            int[] arr = que.poll();
+            int i = arr[0];
+            int j = arr[1];
+            health = arr[2];
+            
+            if (i < 0 || j < 0 || i >= m || j >= n) continue;
+            if(grid.get(i).get(j) == 1) health--;
+            
+            if(health <= 0) continue;
+            if(i == m - 1 && j == n - 1) return true;
+
+            if(healthMap[i][j] < health) healthMap[i][j] = health;
+            else continue; 
+            
+
+
+            que.add(new int[] {i + 1, j, health});
+            que.add(new int[] {i, j + 1, health});
+            que.add(new int[] {i - 1, j, health});
+            que.add(new int[] {i, j -1, health});
+        
+        }
+
+        return false;
+    }
+}
