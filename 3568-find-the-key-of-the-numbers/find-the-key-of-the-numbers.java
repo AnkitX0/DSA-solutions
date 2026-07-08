0dece324a2574ee63159a4b6eb1e03aa70a85846
Ankit Singh <ankitsingh10a@gmail.com>
2026-07-08 09:58:47 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (80.90%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.java
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.java
@@ -2,14 +2,15 @@ class Solution {
     public int generateKey(int num1, int num2, int num3) {
         
         int num = 0;
-        int i = 0;
+        int i = 1;
         while(num1 > 0 && num2 > 0 && num3 > 0){
             int mini = ((int) Math.min(num1 % 10, Math.min(num2 % 10, num3 % 10)));
             num1 /= 10;
             num2 /= 10;
             num3 /= 10;
 
-            num += (int)Math.pow(10, i++) * mini;
+            num +=  i * mini;
+            i *= 10;
         }
         return num;
     }
