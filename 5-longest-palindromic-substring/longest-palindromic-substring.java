69047f5cf6e282f26ffacf3d8180f238e74c4bef
Ankit Singh <ankitsingh10a@gmail.com>
2026-07-11 11:13:02 +0530

    Time: 19 ms (41.15%) | Memory: 46.6 MB (38.23%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,35 +1,32 @@
 class Solution {
-    int n;
-    String str;
-
     public String longestPalindrome(String s) {
-        n = s.length();
-        if(n==1) return s;
-        String maxStr = "";
-        str = s;
+        
         StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < s.length(); i++){
 
-        for(int i=0; i<n-1; i++){
-            palin(i,i+1,sb);
-            if(sb.length() > maxStr.length()) maxStr = sb.toString();
-            sb.delete(0, sb.length());
+            int left = i;
+            int right = i + 1;
+
+            while(left >= 0 && right < s.length() && (s.charAt(left) == s.charAt(right))){
+                left--;
+                right++;
+            }
             
-            sb.append(s.charAt(i));
-            palin(i-1,i+1,sb);
-            if(sb.length() > maxStr.length()) maxStr = sb.toString();
-            sb.delete(0, sb.length());
+            if(sb.length() < right -left - 1) sb = new StringBuilder(s.substring(left + 1, right));
+
+            left = i;
+            right = i;
+
+            while(left >= 0 && right < s.length() && (s.charAt(left) == s.charAt(right))){
+                left--;
+                right++;
+            }
             
+            if(sb.length() < (right -left - 1)) sb = new StringBuilder(s.substring(left + 1, right));           
+
         }
 
-        return maxStr;
-    }
-
-    private void palin(int left, int right, StringBuilder sb){
-        if(left<0 || right >= n || str.charAt(left)!= str.charAt(right)) return;
-
-        sb.insert(0,str.charAt(left));
-        sb.append(str.charAt(right));
-
-        palin(left-1, right+1, sb);
+        return sb.toString();
     }
 }
