39693ec01df3245bcab0d3af7b599ecdc4dd5eff
Ankit Singh <ankitsingh10a@gmail.com>
2026-07-14 13:51:56 +0530

    Time: 6 ms (21.74%) | Memory: 44.1 MB (16.95%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        
+        int bullCounter = 0;
+
+        HashMap<Integer, Integer> secretMap = new HashMap<>();
+        HashMap<Integer, Integer> guesstMap = new HashMap<>();
+
+        for(int i = 0 ; i < secret.length(); i++){
+            int se = secret.charAt(i) - '0';
+            int ge = guess.charAt(i) - '0';
+            if( se == ge) bullCounter++;
+            else {
+                secretMap.put(se, secretMap.getOrDefault(se, 0) + 1);
+                guesstMap.put(ge, guesstMap.getOrDefault(ge, 0) + 1);
+            }
+        }
+        int guessCounter = 0;
+
+        for(int key : guesstMap.keySet()){
+
+            if(secretMap.containsKey(key)) {
+                guessCounter += Math.min(guesstMap.get(key), secretMap.get(key));
+            }
+        }
+
+        return String.valueOf(bullCounter) + "A" + String.valueOf(guessCounter) +"B";
+    }
+}
