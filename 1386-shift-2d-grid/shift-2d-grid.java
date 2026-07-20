c2dc62d5a4d972d5283c8e55a7bd40e3000c33c2
Ankit Singh <ankitsingh10a@gmail.com>
2026-07-20 10:39:21 +0530

    Time: 6 ms (55.40%) | Memory: 47.2 MB (44.14%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        
+        while(k-- > 0){
+
+            int temp  = grid[grid.length - 1][grid[0].length-1];
+
+            for(int i = 0; i < grid.length; i++){
+                for(int j = 0; j < grid[0].length; j++){
+                    int val = grid[i][j];
+                    grid[i][j] = temp;
+                    temp = val;
+                }
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int row[] : grid){
+            List<Integer> r = new ArrayList<>();
+            for(int i : row){
+                r.add(i);
+            }
+            ans.add(r);
+        }
+
+        return ans;
+    }
+}
