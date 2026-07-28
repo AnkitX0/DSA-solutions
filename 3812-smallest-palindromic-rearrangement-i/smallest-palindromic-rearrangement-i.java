225929c364b9e0c6c6707f9c6c907ccc7deaa715
Ankit Singh <ankitsingh10a@gmail.com>
2026-07-28 08:35:05 +0530

    Time: 22 ms (91.16%) | Memory: 48.4 MB (23.69%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,40 +1,30 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        
+
+        int n = s.length();
         int arr[] = new int[26];
+        char res[] = new char[n];
+
         for(char ch : s.toCharArray()){
             arr[ch-'a']++;
         }
-        StringBuilder sb = new StringBuilder();
-        String mid = "";
 
+        int left = 0;
+        int right = n - 1;
+        for(int i = 0; i < 26; i++){
+            
+            char ch = (char)('a' + i);
+            while(arr[i] >= 2){
+                res[left++] = ch;
+                res[right--] = ch;
+                arr[i] -= 2;
+            }
 
-        // first half or palindrome
-        for(int i = 0; i < 26; i++)
-        {
-            if( arr[i] > 0 && arr[i] % 2 != 0)  {
-                mid = String.valueOf((char)('a' + i));
+            if(arr[i] == 1){
+                res[n / 2] = ch;
                 arr[i]--;
             }
-            
-            if(arr[i] > 0){
-                int val = arr[i]/2;
-                while(arr[i] > val){
-                    sb.append( (char)('a' + i));
-                    arr[i]--;
-                }
-            }
         }
-        sb.append(mid);
-        
-        // second half of string
-        for(int i = 25; i >= 0; i--){
-            if(arr[i] > 0){
-                while(arr[i]-- > 0){
-                    sb.append( (char)('a' + i));
-                }
-            }
-        }
-        return sb.toString();
+        return new String (res);
     }
 }
