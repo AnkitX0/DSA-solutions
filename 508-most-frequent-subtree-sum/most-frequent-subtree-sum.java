045b74161ae12392a8fb916cc3319e44e37ed0ac
Ankit Singh <ankitsingh10a@gmail.com>
2026-08-11 19:43:59 +0530

    Time: 4 ms (89.18%) | Memory: 47.2 MB (77.28%) - LeetSync

--- 508-most-frequent-subtree-sum/most-frequent-subtree-sum.java
+++ 508-most-frequent-subtree-sum/most-frequent-subtree-sum.java
@@ -14,44 +14,36 @@
  * }
  */
 class Solution {
-    HashMap<Integer, Integer> map;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int maxFreq = 0;
+
     public int[] findFrequentTreeSum(TreeNode root) {
         if(root == null) return new int[]{};
-        map = new HashMap<>();
 
         traverse(root);
 
-     Map<Integer, Integer> sortedMap = map.entrySet()
-        .stream()
-        .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-        .collect(Collectors.toMap(
-                Map.Entry::getKey,
-                Map.Entry::getValue,
-                (e1, e2) -> e2,
-                LinkedHashMap::new
-        ));
-
         ArrayList<Integer> list = new ArrayList<>();
-        int firstKey = sortedMap.keySet().iterator().next();
-        int maxValue = sortedMap.get(firstKey);
-        for(int i : sortedMap.keySet()){
-            if(maxValue != sortedMap.get(i)) break;
-            list.add(i);
-        }        
-        int result[] = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            result[i] = list.get(i);
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() == maxFreq) list.add(entry.getKey());
         }
-        return result;
+        int ans[] = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++) ans[i] = list.get(i);
+
+        return ans;
+
     }
 
-    public int traverse(TreeNode root){
+    public int traverse (TreeNode root){
         if(root == null) return 0;
 
-        int num = root.val + traverse(root.right)  + traverse(root.left);
-        map.put(num, map.getOrDefault(num, 0) + 1);
+        int sum = root.val + traverse(root.left) + traverse(root.right);
 
-        return num;
-        
+        int freq = map.getOrDefault(sum, 0) + 1;
+        map.put(sum, freq);
+
+        maxFreq = Math.max(freq, maxFreq);
+
+        return sum;
     }
 }
