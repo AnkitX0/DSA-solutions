758b7d7b3ad8facef9c89dbc8dc4a5f328103712
Ankit Singh <ankitsingh10a@gmail.com>
2026-08-29 16:01:33 +0530

    Time: 3 ms (76.78%) | Memory: 43.3 MB (16.60%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,41 @@
+class Pair{
+    char ch;
+    int count;
+    public Pair(char ch, int count){
+        this.ch = ch;
+        this.count = count;
+    }
+}
+class Solution {
+    public String reorganizeString(String s) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>( (a,b) -> b.count - a.count);
+        int[] map = new int[26];
+
+        for(char ch : s.toCharArray()){
+            map[ch-'a']++;
+            if(map[ch-'a'] > (s.length()+1)/2) return "";
+        }
+        for(int i = 0; i < 26; i++){
+            if(map[i] > 0){
+                pq.add(new Pair((char)('a'+i), map[i]));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(pq.size() > 1){
+            Pair p1 = pq.poll();
+            Pair p2 = pq.poll();
+
+            sb.append(p1.ch);
+            sb.append(p2.ch);
+
+            if(p1.count > 1) pq.add(new Pair(p1.ch, p1.count-1));
+            if(p2.count > 1) pq.add(new Pair(p2.ch, p2.count-1));
+        }
+        if(!pq.isEmpty()){Pair p2 = pq.poll();
+        sb.append(p2.ch);}
+
+        return sb.toString();
+    }
+}
