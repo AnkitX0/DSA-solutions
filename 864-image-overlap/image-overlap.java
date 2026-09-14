7d7bff8db41001c920e37916b02aed8f089b7492
Ankit Singh <ankitsingh10a@gmail.com>
2026-09-14 14:10:04 +0530

    Time: 62 ms (41.45%) | Memory: 44.3 MB (85.55%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -4,53 +4,6 @@ class Solution {
         int n = img2.length;
         int maxCounter = 0;
 
-        // // 1 bits left
-        // for (int idx = n-1; idx >= 0; idx--) {
-        //     counter = 0;
-        //     for (int i = 0; i < m; i++){
-        //         for (int j = 0; j < n; j++){
-        //             if (idx+j >= n) break;
-        //             if (img1[i][j] == 1 && 1 == img2[i][j+idx]) counter++;
-        //         }
-        //     }
-        //     maxCounter = maxCounter < counter ? counter : maxCounter;
-        // }    
-        // // 1 bit right
-        // for(int idx = 1; idx < n; idx++){
-        //     counter = 0;
-        //     for (int i = 0; i < m; i++){
-        //         for (int j = 0; j < n; j++){
-        //             if (j-idx < 0) break;
-        //             if (img1[i][j] == 1 && 1 == img2[i][j-idx]) counter++;
-        //         }
-        //     }
-        //     maxCounter = maxCounter < counter ? counter : maxCounter;
-        // }
-
-        // // 1 bits up
-        // for (int idx = m-1; idx >= 0; idx--) {
-        //     counter = 0;
-        //     for (int i = 0; i < m; i++){
-        //         if (idx+i >= m) break;
-        //         for (int j = 0; j < n; j++){
-        //             if (img1[i][j] == 1 && 1 == img2[i + idx][j]) counter++;
-        //         }
-        //     }
-        //     maxCounter = maxCounter < counter ? counter : maxCounter;
-        // }    
-        // // 1 bit down
-        // for(int idx = 1; idx < m; idx++){
-        //     counter = 0;
-        //     for (int i = 0; i < m; i++){
-        //         if (i-idx < 0) break;
-        //         for (int j = 0; j < n; j++){
-        //             if (img1[i][j] == 1 && 1 == img2[i - idx][j]) counter++;
-        //         }
-        //     }
-        //     maxCounter = maxCounter < counter ? counter : maxCounter;
-        // }
-        
-
         for (int row = -(n-1); row < n; row++){
             for(int col = -(n-1); col < n; col++){
 
