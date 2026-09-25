ea0c380c699fcba9d89cfed06652c0b6f5680010
Ankit Singh <ankitsingh10a@gmail.com>
2026-09-25 13:17:11 +0530

    Time: 5 ms (7.30%) | Memory: 45.9 MB (32.91%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,16 +1,11 @@
 class Solution {
-    HashSet<List<Integer>> set = new HashSet<>();
     List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         if(candidates[0] > target) return new ArrayList<>();
 
-        for (int i = 0; i < candidates.length; i++) {
-            backtrack (i, candidates, target, new ArrayList<>(), 0);
-        }
-        for (List<Integer>lis : set){
-            result.add(lis);
-        }
+        backtrack (0, candidates, target, new ArrayList<>(), 0);
+
         return result;        
     }
 
@@ -18,7 +13,7 @@ class Solution {
         
         if (sum > target || i >= cand.length) return;
         if (sum == target){
-            set.add(new ArrayList<>(lis));
+            result.add(new ArrayList<>(lis));
             return;
         }
 
