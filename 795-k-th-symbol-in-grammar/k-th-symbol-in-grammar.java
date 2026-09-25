d1efb5bcb014f7d9139571c04d4609f0cd849bda
Ankit Singh <ankitsingh10a@gmail.com>
2026-09-25 09:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (57.07%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if( n == 1 || k == 1) return 0;
+
+        int num = kthGrammar(n-1,  (k+1) /2);
+        if( k % 2 == 0){
+            num = (num+1)%2;
+        }
+        return num;
+    }
+}
